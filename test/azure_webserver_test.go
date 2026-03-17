c3bd685465b6dbe1bbd1f181478eb3137f68da28
Arpit Patel <pate1562@algonquinlive.com>
2026-03-17 17:25:28 -0400

    Update Terratest variables including subscription ID and label prefix

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "69be1f0a-ef3f-4038-96d6-3f6d028fd393"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "pate1562",
 		},
 	}
 
